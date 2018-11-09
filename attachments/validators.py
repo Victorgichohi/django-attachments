0334f7b0251731a412f0f203c84fcac8f76a7a2b
victor gichohi <victorgichohi48@gmail.com>
2018-11-09 22:20:08 +0300

    rolledback commit, resetting pointer after virus scan

--- attachments/validators.py
+++ attachments/validators.py
@@ -6,13 +6,6 @@ from django.conf import settings
 
 # Validate file type using python magic and scan for virus using python clamd
 def validate_file_type(upload):
-    # setup unix socket and scan stream
-    cd = clamd.ClamdNetworkSocket(settings.CLAMD_TCP_ADDR, settings.CLAMD_TCP_SOCKET)
-    scan_results = cd.instream(upload)
-    if (scan_results['stream'][0] == 'FOUND'):
-        return False
-
-    # if file not infected check whether we allow it
     upload.seek(0)
     file_type = magic.from_buffer(upload.read(), mime=True)
     allowed_types = settings.ALLOWED_FILE_TYPES
@@ -20,4 +13,11 @@ def validate_file_type(upload):
         raise ValidationError('You cannot upload file type: {}. Allowed file types are: {}.'
                                   .format(str(file_type), ', '.join(allowed_types.viewkeys())))
 
-
+    else:
+        # setup unix socket and scan stream
+        cd = clamd.ClamdNetworkSocket(settings.CLAMD_TCP_ADDR, settings.CLAMD_TCP_SOCKET)
+        upload.seek(0)
+        scan_results = cd.instream(upload)
+        upload.seek(0)
+        if (scan_results['stream'][0] == 'FOUND'):
+            raise ValidationError("Your file appears to be infected by a virus.Please check again before uploading.")
